0e33f1c9230f56e5248ce79e11e7e947dda3e1a9
박정민 <v5833165v@naver.com>
2024-01-19 21:18:53 +0900

    first commit

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
+
 // import { FriendStatus } from 'src/friends/entity/friendStatus';
 
 class RelationDto {
