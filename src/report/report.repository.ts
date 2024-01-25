0e9f31a4a991a51c09cd9dcc0129099f5f08a0f1
[gunryul] <[gun9311@gmail.com]>
2024-01-25 10:49:35 +0900

    recent

--- src/report/report.repository.ts
+++ src/report/report.repository.ts
@@ -1,48 +1,47 @@
-import { Injectable } from "@nestjs/common";
-import { BaseEntity, DataSource, Repository } from "typeorm"
-import { Report } from "./entities/report.entity";
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Report } from './entities/report.entity';
 
 @Injectable()
-export class ReportRepository extends Repository<Report>{
-    constructor(private dataSource: DataSource) {
-        super(Report, dataSource.createEntityManager());
-    }
+export class ReportRepository extends Repository<Report> {
+  constructor(private dataSource: DataSource) {
+    super(Report, dataSource.createEntityManager());
+  }
 
+  // async findById(userId: number,fromDate:Date): Promise<object> {
+  //     const latestreport = await this.createQueryBuilder('report')
+  //       .where('report.userId = :userId', { userId })
+  //       .andWhere('report.timestamp > :fromDate', { fromDate })
+  //       .orderBy('report.timestamp', 'DESC') // timestamp를 기준으로 내림차순 정렬
+  //       .select('report.report')
+  //       .getOne();
 
-    // async findById(userId: number,fromDate:Date): Promise<object> {
-    //     const latestreport = await this.createQueryBuilder('report')
-    //       .where('report.userId = :userId', { userId })
-    //       .andWhere('report.timestamp > :fromDate', { fromDate })
-    //       .orderBy('report.timestamp', 'DESC') // timestamp를 기준으로 내림차순 정렬
-    //       .select('report.report')
-    //       .getOne();
+  //     if (!latestreport)  {
+  //         return null;
+  //     }
 
-    //     if (!latestreport)  {
-    //         return null;
-    //     }
-        
-    //     const parsedReport = JSON.parse(latestreport.report);
-    //     return parsedReport;
-    //     // return latestreport.report;
-    // }
+  //     const parsedReport = JSON.parse(latestreport.report);
+  //     return parsedReport;
+  //     // return latestreport.report;
+  // }
 
-    async findById(userId: number): Promise<Report> {
-        const latestReport = await this.findOne({
-            where: { userId },
-            order: { timestamp: 'DESC' }, // timestamp를 기준으로 내림차순 정렬
-            select: {report: true}
-          });
-      
-        return latestReport
-    }
+  async findById(userId: number): Promise<Report> {
+    const latestReport = await this.findOne({
+      where: { userId },
+      order: { timestamp: 'DESC' }, // timestamp를 기준으로 내림차순 정렬
+      select: { report: true },
+    });
 
-    async saveReport(report: string,fromDate:Date): Promise<string> {
-        const reportRecord = this.create({
-            userId: 5,
-            report: report,
-            timestamp: fromDate
-          });
-        this.save(reportRecord);
-        return report;
-    }
+    return latestReport;
+  }
+
+  async saveReport(report: string, fromDate: Date): Promise<string> {
+    const reportRecord = this.create({
+      userId: 5,
+      report: report,
+      timestamp: fromDate,
+    });
+    this.save(reportRecord);
+    return report;
+  }
 }
